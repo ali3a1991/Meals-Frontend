1d460172ff86f36c087142b2ab41b9a51b8c451a
Your Name <ali3ajob@gmail.com>
2023-11-24 12:17:36 +0100

    login & register styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Onboarding from "./components/pages/Onboarding";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import ShoppingList from "./components/pages/ShoppingList";
+import Access from "./components/pages/Access";
+import Favourite from "./components/pages/Favourite";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Onboarding />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/shoppinglist" element={<ShoppingList />} />
+      <Route path="/access" element={<Access />} />
+      <Route path="/favourite" element={<Favourite />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Login() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <div className='bg-gray-300 p-10 rounded-2xl flex flex-col gap-4 w-96 justify-center'>
+        <form className='flex flex-col gap-5'>
+          <input className='rounded-md p-2' type="email" placeholder='Email'/>
+          <input className='rounded-md p-2' type="password" placeholder='Password' />
+          <input className='rounded-md p-2 bg-gray-700 text-white font-bold w-28 mx-auto cursor-pointer' type="submit" value='Login' />
+        </form>
+        <Link className='text-blue-700 underline' to={'/register'}>Register</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Login
